2c6934253d25bb9951d9cc04bdac032097fad517
eklompus <eklompus@student.21-school.ru>
2021-03-27 16:26:03 +0300

    Добавлено скачивание файлов с приложениями

--- init.sh
+++ init.sh
@@ -1,16 +1,58 @@
 #!/bin/bash
-HOMEDIR=$HOME
-GOINFREDIR="$HOMEDIR/goinfre"
-CACHES_DIR="$HOMEDIR/Library/Caches/"
-echo  "logged in $(date)" >> log.txt
-folders=(".m2" ".npm" ".gradle")
-for i in "${folders[@]}"
-do
-	[[ ! -e "$GOINFREDIR/$i" ]] && mkdir "$GOINFREDIR/$i" && echo "dir $i created"
-	[[ ! -e "$HOMEDIR/$i" ]] && ln -s "$GOINFREDIR/$i" ~ && echo "link $i created"
+
+GOINFREDIR="$HOME/goinfre"
+CACHES_DIR="$HOME/Library/Caches/"
+
+idea="https://download.jetbrains.com/idea/ideaIU-2020.3.3.dmg@IntelliJ IDEA.app"
+vlc="https://mirror.yandex.ru/mirrors/ftp.videolan.org/vlc/3.0.11.1/macosx/vlc-3.0.11.1.dmg@VLC.app"
+tor="https://www.torproject.org/dist/torbrowser/10.0.5/TorBrowser-10.0.5-osx64_en-US.dmg@Tor Browser.app"
+
+apps=("${idea}" "${vlc}")
+echo "logged in $(date)" >>log.txt
+folders=(".m2" ".npm" ".gradle" "Applications" "Library/Containers/com.docker.docker" ".sdkman" "Library/Java")
+
+for i in "${folders[@]}"; do
+  [[ ! -e "$GOINFREDIR/$i" ]] && mkdir -p "$GOINFREDIR/$i" && echo "dir $i created"
+  [[ -d "$HOME/$i" ]] && [[ ! -L "$HOME/$i" ]] && rm -rf "${HOME:?}/$i" && echo "$HOME/$i is not a symlink, deleting..."
+  [[ ! -e "$HOME/$i" ]] && ln -s "$GOINFREDIR/$i" "$HOME/$i" && echo "link $i created"
 done
+
 defaults write -g com.apple.swipescrolldirection -bool false
 defaults write -g com.apple.keyboard.fnState -bool true
 defaults write -g TISRomanSwitchState -bool true
-for f in "$CACHES_DIR"*; { rm -rf $f && echo "$f deleted"; }
+
+for f in "$CACHES_DIR"*; do
+  rm -rf "$f" && echo "$f deleted"
+done
+
+for app in "${apps[@]}"; do
+  link=$(echo "$app" | awk -F@ ' { print $1 } ')
+  appname=$(echo "$app" | awk -F@ ' { print $2 } ')
+  file_name=$(echo "$link" | rev | cut -d/ -f1 | rev)
+  if [[ ! -e "$GOINFREDIR/Applications/$appname" ]]; then
+    if [[ ! -e "$GOINFREDIR/$file_name" ]]; then
+      status_code=$(curl --write-out %{http_code} "$link" -o "$GOINFREDIR/$file_name" -L)
+      if [[ "$status_code" -ne 200 ]]; then
+        echo "error download $file_name!!!!"
+        rm -rf "${GOINFREDIR:?}/$file_name" && echo "$GOINFREDIR/$file_name deleted"
+      fi
+    fi
+    if [[ -e "$GOINFREDIR/$file_name" ]]; then
+      volume_name="$(hdiutil attach "$GOINFREDIR/$file_name" | grep 'Apple_HFS' | grep '.*\t.*\t.*' | awk -F'\t' '{print $3}')"
+      echo "$volume_name"
+      volume_app="$volume_name/$appname"
+      echo "$volume_app"
+      if [[ -e "$volume_app" ]]; then
+        cp -r "$volume_app" "$GOINFREDIR/Applications" && echo "$appname copied"
+      fi
+      hdiutil detach "$volume_name"
+      rm -rf "${GOINFREDIR:?}/$file_name" && echo "$GOINFREDIR/$file_name deleted"
+    fi
+  fi
+done
+
+if [[ ! -e "$HOME/.sdkman/bin" ]] ;then
+  curl -s "https://get.sdkman.io" | sed 's#if \[ -d \"$SDKMAN_DIR\" \]; then#if \[ -d \"$SDKMAN_DIR/bin\" \]; then#g' | bash
+fi
+
 exit 0
